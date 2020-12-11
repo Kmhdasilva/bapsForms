fdf15b492e8a179a948cc80bfaf7fae36ba4037c
Kevin Nunes da Silva <kmh.nunesdasilva@gmail.com>
2020-12-11 15:33:47 +0100

    Button click creates list
    buttons still visible

--- bapsforms/baps.cs
+++ bapsforms/baps.cs
@@ -26,16 +26,36 @@ namespace bapsforms
         private void CreateButtons()
         {
             int top = 50;
-            int left = 100;
+            int left = 20;
 
             foreach(var room in rooms)
             {
                 Button roomButton = new Button();
+                roomButton.Name = room.name;
                 roomButton.Left = left;
                 roomButton.Top = top;
+                roomButton.Height = 150;
+                roomButton.Width = 500;
                 roomButton.Text = room.name;
                 this.Controls.Add(roomButton);
                 top += roomButton.Height + 2;
+                roomButton.Click += (sender, EventArgs) => { ShowRoomInfo(sender, EventArgs, room.id);  };
+
+            }
+        }
+
+        private void ShowRoomInfo(object sender, EventArgs e, int id)
+        {
+            foreach (var room in rooms)
+            {
+                if(room.id == id)
+                {
+                    ListBox roomList = new ListBox();
+                    roomList.Location = new Point(50, 50);
+                    roomList.Items.Add(room.name);
+                    this.Controls.Add(roomList);
+                    roomList.ResumeLayout();
+                }
             }
         }
 
